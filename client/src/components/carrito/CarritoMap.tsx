7b4663a0fa0d87ed2ee265a77bc201b3606e8d12
Luis Ernesto <luisiportal@gmail.com>
2025-11-25 22:07:41 -0500

    (menu)

--- client/src/components/carrito/CarritoMap.tsx
+++ client/src/components/carrito/CarritoMap.tsx
@@ -13,16 +13,19 @@ const CarritoMap = () => {
     <section id="productos" className="flex flex-col gap-5 justify-center">
       {" "}
       <div className="flex flex-col gap-3 m-3 mt-5">
-        {carrito.productos?.length ? carrito?.productos?.map((item) => (
-          <CardCarrito item={item} key={item.id} />
-        )) : <Loader/>}
+        {carrito.productos?.length ? (
+          carrito?.productos?.map((item) => (
+            <CardCarrito item={item} key={item.id} />
+          ))
+        ) : (
+          <Loader />
+        )}
       </div>
-       <div className="relative ml-1 mt-5 flex">
+      <div className="relative ml-1 mt-5 flex">
         <h2 className="ml-1 font-bold text-2xl">Total a Pagar</h2>
         <div className="-z-10 bg-green-500 w-32 h-10 absolute -top-1 rounded-br-xl rounded-tr-xl overflow-visible"></div>{" "}
       </div>
       <section className="bg-neutral-200 rounded-xl p-3 mx-3 min-h-20">
-        
         <div className="flex items-center justify-between">
           <h2 className="font-semibold">Productos :</h2>{" "}
           <h2 className="font-bold text-xl">{carrito.total?.toFixed(2)} USD</h2>
@@ -34,18 +37,19 @@ const CarritoMap = () => {
           </h2>{" "}
           <h2 className="font-bold text-xl">0 USD</h2>
         </div>
-           <div className="flex items-center justify-between">
-          <h2 className="font-semibold">
-            Total a Pagar :{" "}
-            
-          </h2>{" "}
-          <h2 className="font-bold text-xl bg-neutral-100 p-0.5 rounded-xl">{carrito?.total?.toFixed(2)} USD</h2>
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold">Total a Pagar : </h2>{" "}
+          <h2 className="font-bold text-xl bg-neutral-100 p-0.5 rounded-xl">
+            {carrito?.total?.toFixed(2)} USD
+          </h2>
         </div>
       </section>
-      <MetodoPago/>
-
+      <MetodoPago />
+      <div className="font-bold text-sm flex items-center bg-neutral-200 rounded-xl p-3 mx-3">
+        <img className="size-10" src="/svg/clock.svg" alt="Entregas Puntual" />
+        Entregas en menos de 24 horas
+      </div>
       <Entregar carrito={carrito} />
-
     </section>
   );
 };
