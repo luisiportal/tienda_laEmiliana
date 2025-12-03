0a4dfad4488a1b8f417e3c1fbdd0b512ee685889
Luis Ernesto <luisiportal@gmail.com>
2025-12-03 12:11:14 -0500

    (productos)

--- client/src/categorias.ts
+++ client/src/categorias.ts
@@ -4,48 +4,44 @@ export const categorias = [
   {
     id: 1,
     nombre: "Carnícos",
-    image: "combo1.jpg",
+    image: "pollo10lb.jpg",
     alias: "carnicos",
   },
   {
     id: 2,
     nombre: "Granos",
-    image: "combo1.jpg",
+    image: "frijoles1kg.jpg",
     alias: "granos",
   },
   {
     id: 3,
     nombre: "Confituras",
-    image: "combo1.jpg",
+    image: "milkRedonda.webp",
     alias: "confituras",
   },
-  {
-    id: 4,
-    nombre: "Pastas",
-    image: "combo1.jpg",
-    alias: "pastas",
-  },
-  {
-    id: 5,
-    nombre: "Aceite",
-    image: "combo1.jpg",
-    alias: "aceites",
-  },
+
+ 
   {
     id: 6,
     nombre: "Destacados",
-    image: "combo1.jpg",
-    alias: "destacados",
-  },
+    image: "flan.jpeg",
+    alias: "destacados",},
+  
    {
     id: 7,
     nombre: "Lácteos",
-    image: "combo1.jpg",
+    image: "yogurt-fresa-vima.webp",
     alias: "lacteos",
   },  {
     id: 8,
     nombre: "Varios",
-    image: "combo1.jpg",
+    image: "arroz.jpg",
+    alias: "varios",
+  },
+  {
+    id: 9,
+    nombre: "Aseo",
+    image: "jabon-carbon.jpg",
     alias: "varios",
   },
   
