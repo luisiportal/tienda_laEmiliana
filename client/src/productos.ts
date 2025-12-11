bf6b9987965821525b5b0283c746b3a50ad8ab5e
luisiportal <153392533+luisiportal@users.noreply.github.com>
2025-12-11 16:20:01 -0500

    Update productos.ts

--- client/src/productos.ts
+++ client/src/productos.ts
@@ -150,7 +150,7 @@ export const productos: Producto[] = [
     image: "queso-gouda_3.webp",
     categoria: "Lácteos",
     alias: "queso-gouda-1lb",
-    agotado: true,
+    agotado: false,
   },
   {
     id: 16,
